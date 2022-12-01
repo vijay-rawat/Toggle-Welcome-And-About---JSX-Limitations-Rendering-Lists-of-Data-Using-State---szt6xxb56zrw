7ede30948a5445e4036fc873e30880cd444c07cd
Vijay Rawat <30477642+vijay-rawat@users.noreply.github.com>
2022-12-01 23:28:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
+
+  const [getFlag,setFlag] = useState(false);
+
+  const onclickHandler=()=>{
+    setFlag(!getFlag);
+  }
+
   return (
     <div id="main">
-      <div id="welcome-div">
+     {getFlag && <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      </div> } 
+      {!getFlag &&
+           <div id="about-div">
+           <h1>
+             Newton School is a neo-university providing highly immersive tech
+             learning to millions of students, to tap into new-age tech
+             opportunities.
+           </h1>
+         </div> 
+      }
+ 
+      <button onClick={onclickHandler} id="toggle">Toggle</button>
     </div>
   );
 };
